05a2c10ca21a7f913cbfb8496dff6be0044ce059
KazukiAmatatsu <80532711+KazukiAmatatsu@users.noreply.github.com>
2021-07-13 23:57:27 +0900

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,27 @@
-import React from "react";
-import reactDom from "react-dom";
-import { firebase } from "./firebase";
+import React from 'react';
+import { firebase } from './firebase';
 
 function App() {
   // firebase.firestore(); //firestoreを使う
   // firebase.auth(); //認証機能を使う
 
-  const [input, setInput] = React.useState();
+  const [input, setInput] = React.useState('');
+  const [data, setData] = React.useState([]);
 
   // 追加
+  // const add = () => {
+  //   firebase.firestore().collection('collectionA').add({
+  //addするとランダムIDで追加する
+  //     title: 'ボタン押した',
+  //     num: 123,
+  //     bool: false,
+  //     text: input,
+  //   });
+  // };
   const add = () => {
-    firebase.firestore().collection("collectionA").add({
-      title: "ボタン押した",
+    firebase.firestore().collection('collectionA').doc('idddd').set({
+      //setするとその中身になる
+      title: 'ボタン押した',
       num: 123,
       bool: false,
       text: input,
@@ -20,8 +30,8 @@ function App() {
 
   // 削除
   const remove = () => {
-    firebase.firestore().collection("collectionA").add({
-      title: "ボタン押した",
+    firebase.firestore().collection('collectionA').doc('idddd').delete()({
+      title: 'ボタン押した',
       num: 123,
       bool: false,
     });
@@ -29,21 +39,86 @@ function App() {
 
   // 更新
   const update = () => {
-    firebase.firestore().collection("collectionA").add({
-      title: "ボタン押した",
-      num: 123,
-      bool: false,
+    firebase.firestore().collection('collectionA').doc('idddd').update({
+      text: 'テキストだけ書き換えたよ',
     });
   };
+  // const update = () => {
+  //   firebase.firestore().collection('collectionA').doc('idddd').set({
+  //     title: 'ボタン押した',
+  //     num: 123,
+  //     bool: false,
+  //     text: 'テキスト以外も設定したよ',
+  //   });
+  // };
+
+  // 取得
+  const getData = () => {
+    firebase
+      .firestore()
+      .collection('collectionA')
+      .doc('idddd')
+      .get()
+      .then((doc) => {
+        console.log(doc.data());
+        console.log(doc.id);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+
+  // 全取得
+  const getAllData = async () => {
+    // firebase
+    // .firestore()
+    // .collection('collectionA')
+    // .get()
+    // .then((querySnapshot) => {
+    //   querySnapshot.forEach((doc) => {
+    //     console.log(doc.data());
+    //   });
+    // })
+    // .catch((err) => {
+    //   console.log(err);
+    // });
+    const fetchData = await firebase
+      .firestore()
+      .collection('collectionA')
+      .get();
+    const fetchDatas = fetchData.docs.map((doc) => doc.data());
+    setData(fetchDatas);
+    // console.log(fetchData);
+    // console.log(fetchDatas);
+  };
+
+  // データの監視
+  const observe = () => {
+    firebase
+      .firestore()
+      .collection('collectionA')
+      .onSnapshot((snapshot) => {
+        const fetchDatas = snapshot.docs.map((doc) => doc.data());
+        setData(fetchDatas);
+      });
+  };
+
+  console.log(data);
 
   return (
     <div>
       <h1>firebase </h1>
-      <input type="text" value={input} onChange={(e) => setInput(e.target.value)} />
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
       <button onClick={add}>追加</button>
       <button onClick={remove}>削除</button>
       <button onClick={update}>更新</button>
-      <button onClick={add}>追加</button>
+      <button onClick={getData}>取得</button>
+      <button onClick={getAllData}>全取得</button>
+      <button onClick={observe}>監視</button>
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
-import firebase from "firebase/app";
-import "firebase/firestore";
-import "firebase/auth";
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
 const firebaseConfig = {
-  apiKey: "AIzaSyARxtIAEMEYhtGLwvDPynVgjPywCGAIsOc",
-  authDomain: "fir-lesson-85bc3.firebaseapp.com",
-  projectId: "fir-lesson-85bc3",
-  storageBucket: "fir-lesson-85bc3.appspot.com",
-  messagingSenderId: "1040830306636",
-  appId: "1:1040830306636:web:36ad0f18589ca5b11c97e9",
+  apiKey: 'AIzaSyARxtIAEMEYhtGLwvDPynVgjPywCGAIsOc',
+  authDomain: 'fir-lesson-85bc3.firebaseapp.com',
+  projectId: 'fir-lesson-85bc3',
+  storageBucket: 'fir-lesson-85bc3.appspot.com',
+  messagingSenderId: '1040830306636',
+  appId: '1:1040830306636:web:36ad0f18589ca5b11c97e9',
 };
 
-if (!firebase.apps.length) {
-  //initializeが何度も行われてエラーが出るときはこれ
-  firebase.initializeApp(firebaseConfig);
-}
+//initializeが何度も行われてエラーが出るときはこれ
+// if (!firebase.apps.length) {
+firebase.initializeApp(firebaseConfig);
+// }
 
 export const db = firebase.firestore();
 export { firebase };
